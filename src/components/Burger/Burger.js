3a3e574f48ce65c20eb67372872c798f46776bbf
David Vivó <david.vivo@oramon.nl>
2020-01-12 12:10:54 +0100

    Create logic to outputting ingredients from BurgerBuilder.
    We need maping the amount into an array from the state object passed as a prop.
    
    We care about the indexes because give us the type of ingredient and the amount.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+                return <BurgerIngredient key={ingredientKey + index} type={ingredientKey}/>
+            });
+        });
+            
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
