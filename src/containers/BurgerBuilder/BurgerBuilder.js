79be8917305984a2c0da37dae6eb1f48756ba18c
David Vivó <david.vivo@oramon.nl>
2020-02-02 18:27:17 +0100

    Create and apply spinner
    Pay special attention to modal lifecycle hooks.
    
    Modal updates when is shown OR when its children change (order summary vs spinner)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -29,7 +30,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchaseable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState (ingredients) {
@@ -101,6 +103,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue');
+        this.setState( { loading: true} )
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -117,8 +120,12 @@ class BurgerBuilder extends Component {
         }
 
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+            .then( response => {
+                this.setState({ loading: false, purchasing: false })
+            })
+            .catch(error => 
+                this.setState({ loading: false, purchasing: false })
+            );
     }
     
     render () {
@@ -134,14 +141,20 @@ class BurgerBuilder extends Component {
         // then transforms values from integer to boolean
         // so ingredients:{ salad: false, bacon: true ...and so on}
 
+        let orderSummary = <OrderSummary 
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}/>
+
+        if (this.state.loading) {
+            orderSummary = <Spinner />
+        }
+
         return (
             <Auxiliary>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
-                        purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}/>
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
